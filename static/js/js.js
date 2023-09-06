d04dc2a7f8ae770c8f417b88b660489854193fa8
Bliss Nwokolo Anthony <blissnwokoloanthoy@gmail.com>
2023-09-06 19:35:15 +0100

    fourth_commit

--- static/js/js.js
+++ static/js/js.js
@@ -5,16 +5,21 @@ function subcribe(){
   alert("thanking for subcribing to ptoject X")
 }
 
-const header = document.querySelector('header');
-header.style.left = "-1000px";
-document.querySelector('div.menu').addEventListener('click', ()=>{
-  if (header.style.left === "-1000px"){
-    header.style.left = "0";
-    header.querySelector('.fa-times').style.display = "block";
-    header.querySelector('.fa-bars').style.display = "none";
-  }else{
+window.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth < 480) {
+    const header = document.querySelector("header");
     header.style.left = "-1000px";
-    header.querySelector('.fa-times').style.display = "none";
-    header.querySelector('.fa-bars').style.display = "block";
+
+    document.querySelector("div.menu").addEventListener("click", () => {
+      if (header.style.left === "-1000px") {
+        header.style.left = "0";
+        header.querySelector(".fa-times").style.display = "block";
+        header.querySelector(".fa-bars").style.display = "none";
+      } else {
+        header.style.left = "-1000px";
+        header.querySelector(".fa-times").style.display = "none";
+        header.querySelector(".fa-bars").style.display = "block";
+      }
+    });
   }
-})
+});
