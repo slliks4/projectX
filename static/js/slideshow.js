d41340e5fa99b486d8a7a9b9e4de724b86bbdf0d
Bliss Nwokolo Anthony <blissnwokoloanthoy@gmail.com>
2023-09-08 08:28:29 +0100

    fourth push

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,7 +1,7 @@
 const slideshow = document.querySelector('.gallery div.slide_container');
 const slides = document.querySelectorAll(".slides");
 
-const slideWidth = slides[0].offsetWidth + parseFloat(getComputedStyle(slides[0]).marginLeft);
+const slideWidth = slides[0].offsetWidth + parseInt(getComputedStyle(slides[0]).marginLeft);
 const slidesToScroll = 1;
 const scrollDistance = slidesToScroll * slideWidth;
 document.querySelector('.slideshow button.left').addEventListener('click', async(e)=>{
